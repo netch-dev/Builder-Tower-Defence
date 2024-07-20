d98783403073f76efc36c4df6474f70f3bbe3e70
netch-dev <daijeln361@gmail.com>
2024-07-20 07:40:04 -0700

    Added scriptable objects to store building data

--- Assets/Scripts/BuildingManager.cs
+++ Assets/Scripts/BuildingManager.cs
@@ -3,18 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BuildingManager : MonoBehaviour {
-	[SerializeField] private Transform woodHarvesterPrefab;
-
 	private Camera mainCamera;
+	private BuildingTypeListSO buildingTypeList;
+	private BuildingTypeSO buildingType;
 
 	private void Start() {
 		mainCamera = Camera.main;
+
+		buildingTypeList = Resources.Load<BuildingTypeListSO>(typeof(BuildingTypeListSO).Name);
+		buildingType = buildingTypeList.list[0];
 	}
 
 	private void Update() {
 		if (Input.GetMouseButtonDown(0)) {
 			Vector3 mouseWorldPostion = GetMouseWorldPosition();
-			Instantiate(woodHarvesterPrefab, mouseWorldPostion, Quaternion.identity);
+			Instantiate(buildingType.prefab, mouseWorldPostion, Quaternion.identity);
+		}
+
+		if (Input.GetKeyDown(KeyCode.T)) {
+			buildingType = buildingTypeList.list[0];
+		}
+
+		if (Input.GetKeyDown(KeyCode.Y)) {
+			buildingType = buildingTypeList.list[1];
 		}
 	}
 
