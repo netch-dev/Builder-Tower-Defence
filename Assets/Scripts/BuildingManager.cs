1ee0890ad6324173eabcacb9a3853f99d3be60e6
netch-dev <daijeln361@gmail.com>
2024-07-21 00:31:41 -0700

    Added scriptable objects for the resource types

--- Assets/Scripts/BuildingManager.cs
+++ Assets/Scripts/BuildingManager.cs
@@ -7,11 +7,14 @@ public class BuildingManager : MonoBehaviour {
 	private BuildingTypeListSO buildingTypeList;
 	private BuildingTypeSO buildingType;
 
-	private void Start() {
-		mainCamera = Camera.main;
 
+	private void Awake() {
 		buildingTypeList = Resources.Load<BuildingTypeListSO>(typeof(BuildingTypeListSO).Name);
 		buildingType = buildingTypeList.list[0];
+	}
+
+	private void Start() {
+		mainCamera = Camera.main;
 	}
 
 	private void Update() {
