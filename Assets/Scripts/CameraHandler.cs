19b264db0f668c1560b2ea312ba428a4ca6d455b
netch-dev <daijeln361@gmail.com>
2024-07-21 02:21:50 -0700

    Added cinemachine and a basic CameraHandler for camera movement and zoom

---
+++ Assets/Scripts/CameraHandler.cs
@@ -0,0 +1,44 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraHandler : MonoBehaviour {
+	[SerializeField] private CinemachineVirtualCamera cinemachineVirtualCamera;
+
+	private float orthographicSize;
+	private float targetOrthographicSize;
+	private void Start() {
+		orthographicSize = cinemachineVirtualCamera.m_Lens.OrthographicSize;
+		targetOrthographicSize = orthographicSize;
+	}
+	private void Update() {
+		HandleMovement();
+
+		HandleZoom();
+	}
+
+	private void HandleMovement() {
+		float x = Input.GetAxisRaw("Horizontal");
+		float y = Input.GetAxisRaw("Vertical");
+
+		Vector3 moveDir = new Vector3(x, y).normalized;
+		float moveSpeed = 30f;
+		transform.position += moveDir * moveSpeed * Time.deltaTime;
+	}
+
+	private void HandleZoom() {
+		float zoomAmount = 2f;
+		targetOrthographicSize += -Input.mouseScrollDelta.y * zoomAmount;
+
+		float minOrthographicSize = 10f;
+		float maxOrthographicSize = 30f;
+		targetOrthographicSize = Mathf.Clamp(targetOrthographicSize, minOrthographicSize, maxOrthographicSize);
+
+		// Lerp to target orthographic size to make it smooth
+		float zoomSpeed = 5f;
+		orthographicSize = Mathf.Lerp(orthographicSize, targetOrthographicSize, Time.deltaTime * zoomSpeed);
+
+		cinemachineVirtualCamera.m_Lens.OrthographicSize = orthographicSize;
+	}
+}
