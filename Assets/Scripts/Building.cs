8f577e06275bcae0978aeb316a36d1e3dd421f4c
netch-dev <daijeln361@gmail.com>
2024-07-24 10:01:17 -0700

    Buildings - Added particles on build and death

--- Assets/Scripts/Building.cs
+++ Assets/Scripts/Building.cs
@@ -38,8 +38,9 @@ public class Building : MonoBehaviour {
 	}
 
 	private void HealthSystem_OnDie(object sender, EventArgs e) {
-		Destroy(gameObject);
 		SoundManager.Instance.PlaySound(SoundManager.Sound.BuildingDestroyed);
+		Instantiate(Resources.Load<Transform>("pfBuildingDestroyedParticles"), transform.position, Quaternion.identity);
+		Destroy(gameObject);
 	}
 
 	private void OnMouseEnter() {
--- Assets/Scripts/BuildingConstruction.cs
+++ Assets/Scripts/BuildingConstruction.cs
@@ -23,6 +23,8 @@ public class BuildingConstruction : MonoBehaviour {
 		spriteRenderer = transform.Find("sprite").GetComponent<SpriteRenderer>();
 		buildingTypeHolder = GetComponent<BuildingTypeHolder>();
 		constructionMaterial = spriteRenderer.material;
+
+		Instantiate(Resources.Load<Transform>("pfBuildingPlacedParticles"), transform.position, Quaternion.identity);
 	}
 
 	private void Update() {
@@ -32,6 +34,7 @@ public class BuildingConstruction : MonoBehaviour {
 		if (constructionTimer <= 0) {
 			Instantiate(buildingType.prefab, transform.position, Quaternion.identity);
 			SoundManager.Instance.PlaySound(SoundManager.Sound.BuildingPlaced);
+			Instantiate(Resources.Load<Transform>("pfBuildingPlacedParticles"), transform.position, Quaternion.identity);
 			Destroy(gameObject);
 		}
 	}
